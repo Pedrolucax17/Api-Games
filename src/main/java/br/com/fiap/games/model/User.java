8a30047daea1ff4659cdb5bf82f02c387b0977f9
Pedro Lucas <pl.lucassouza@gmail.com>
2024-08-01 11:48:56 -0300

    Implementing the user class, and updating the database with the creation of tbl_user

---
+++ src/main/java/br/com/fiap/games/model/User.java
@@ -0,0 +1,23 @@
+package br.com.fiap.games.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tbl_user")
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+}
